d9b97e170357b2c0a4b1efcaf0f3202313cfd484
Cooper Hurt <cooperhurt@Coopers-MacBook-Pro.local>
2022-11-24 17:18:04 -0700

    Update package dependencies and remove rollup

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,6 @@
-/**
- * @name helloWorld
- * @author Cooper Hurt
- * @param { string } name This is the username that you will want to say hello to
- * @returns { string } This is the string gretting the user.
- */
-export const helloWorld = (name: string) => `Hello, ${name}`;
+type User = {
+  name: string;
+  age: number;
+};
+
+export const helloWorld = (user: User) => `Hello, ${user.name}`;
